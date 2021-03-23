f4aa5fd311bc6571d046a12c00379bfdff50e717
Felipe Belinassi <felipebelinassi@gmail.com>
2021-03-23 23:53:48 +0100

    Fix create subject test

--- tests/unit/resolvers/subject/createSubject.spec.ts
+++ tests/unit/resolvers/subject/createSubject.spec.ts
@@ -31,18 +31,18 @@ describe('Create subject mutation unit tests', () => {
 
     const resolver = new CreateSubjectResolver();
     const response = await resolver.createSubject(fakeSubject, fakeContext);
-    expect(createSubjectSpy).toHaveBeenCalledWith(fakeSubject);
+    expect(createSubjectSpy).toHaveBeenCalledWith(mockAuthContext.id, fakeSubject);
     expect(response).toEqual(expectedResponse);
   });
 
-  it('should throw an error if subject was already created by the user', async () => {
+  it('should throw SUBJECT_ALREADY_EXISTS if subject was already created by the user', async () => {
     const fakeContext = { user: mockAuthContext, authExpired: false };
     const fakeSubject = 'Test subject';
 
     createSubjectSpy.mockRejectedValue({ code: 'P2002' });
 
     const resolver = new CreateSubjectResolver();
-    const response = await resolver.createSubject(fakeSubject, fakeContext);
+    const response = resolver.createSubject(fakeSubject, fakeContext);
     await expect(() => response).rejects.toThrow('SUBJECT_ALREADY_EXISTS');
   });
 });
