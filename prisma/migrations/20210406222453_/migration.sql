88edd140323c93b929538f59b31643786cbc5161
Felipe Belinassi <felipebelinassi@gmail.com>
2021-04-12 23:03:11 +0200

    Update migrations and prisma schema

--- prisma/migrations/20210406222453_/migration.sql
+++ prisma/migrations/20210406222453_/migration.sql
@@ -12,13 +12,14 @@ CREATE TABLE "user" (
 -- CreateTable
 CREATE TABLE "subject" (
     "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
 
     PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "event" (
+CREATE TABLE "repetition" (
     "id" TEXT NOT NULL,
     "identifier" TEXT NOT NULL,
     "subject_id" TEXT NOT NULL,
@@ -37,8 +38,14 @@ CREATE UNIQUE INDEX "user.username_unique" ON "user"("username");
 -- CreateIndex
 CREATE UNIQUE INDEX "user.email_unique" ON "user"("email");
 
--- AddForeignKey
-ALTER TABLE "event" ADD FOREIGN KEY ("subject_id") REFERENCES "subject"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+-- CreateIndex
+CREATE UNIQUE INDEX "Subject_userId_title_un_key" ON "subject"("user_id", "title");
 
 -- AddForeignKey
-ALTER TABLE "event" ADD FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "subject" ADD FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "repetition" ADD FOREIGN KEY ("subject_id") REFERENCES "subject"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "repetition" ADD FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
